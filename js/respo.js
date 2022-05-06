8d458789ed034c1e0c95718a51fa168cb947e5ba
Krushnal09 <104764240+Krushnal09@users.noreply.github.com>
2022-05-06 19:20:05 +0530

    first

---
+++ js/respo.js
@@ -0,0 +1,14 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+rightNav = document.querySelector('.rightnav')
+
+burger.addEventListener('click', ()=>
+{
+    rightNav.classList.toggle('v-class')
+    navlist.Class.toggle('v-class');
+    navbar.Class.toggle('h-nav');
+
+}) 
+
+
